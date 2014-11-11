18261372ec2eb4d18ebabccbf710e3eb22979415
Ryan Hanson <hansonry@gmail.com>
2014-11-11 14:45:27 -0600

    Modified build script to work cross platform and include some things for fps cam

--- bam.lua
+++ bam.lua
@@ -7,7 +7,12 @@ if family == "windows" then
    settings.link.flags:Add("/SUBSYSTEM:CONSOLE");   
    settings.link.libs:Add("SDL2main");
    settings.link.libpath:Add("SDL2-2.0.1/lib/x86");
+   settings.link.libs:Add("opengl32");
+   settings.link.libs:Add("glu32");
+   settings.cc.defines:Add("__WIN32__")
 else
+   settings.link.libs:Add("GL");
+   settings.link.libs:Add("GLU");
 end
 
 
