c8efcaa2a3858a0d2740f9db60eb8452c67ec6f3
Ryan Hanson <hansonry@gmail.com>
2015-03-25 14:59:00 -0500

    Basic image loading now works

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+#define TILE_WIDTH             32
+#define TILE_HEIGHT            32
+
+#define IMGID_BLOCK        0x0000
+#define IMGID_BROKENBLOCK  0x0001
+#define IMGID_LADDER       0x0002
+#define IMGID_GOLD         0x0100
+#define IMGID_GUY          0x0101
+#define IMGID_BAR          0x0102
+#define IMGID_DOORCLOSE    0x0200
+#define IMGID_DOOROPEN     0x0201    
 
 static void CheckForExit(const SDL_Event *event, int * done);
+
+static SDL_Texture * load_texture(SDL_Renderer * rend, const char * filename);
+
+static void draw_at(SDL_Renderer * rend, SDL_Texture * text, int imgid, int x, int y);
 
 int main(int args, char * argc[])
 {
    SDL_Window  * window;
    SDL_Renderer * rend;   
    SDL_Event event;
-   SDL_Surface * palet;
+   SDL_Texture * t_palet;
    int done;
    int prevTicks, diffTicks, nowTicks;
    float seconds;
@@ -23,7 +40,7 @@ int main(int args, char * argc[])
    
    rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
    
-   palet = IMG_Load("load.png");
+   t_palet = load_texture(rend, "load.png");
 
    prevTicks = SDL_GetTicks();
    
@@ -42,7 +59,8 @@ int main(int args, char * argc[])
       
       SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0xFF);
       SDL_RenderClear( rend );
-      
+     
+      draw_at(rend, t_palet, IMGID_GUY, 0, 0); 
       SDL_RenderPresent(rend);
    }
    
@@ -51,6 +69,7 @@ int main(int args, char * argc[])
    
    SDL_DestroyRenderer(rend);
    SDL_DestroyWindow(window);
+   SDL_DestroyTexture(t_palet);
    SDL_Quit();
    
    
@@ -73,3 +92,46 @@ static void CheckForExit(const SDL_Event *event, int * done)
    }
 
 }
+
+static SDL_Texture * load_texture(SDL_Renderer * rend, const char * filename)
+{
+   SDL_Surface * surf;
+   SDL_Texture * text;
+   surf = IMG_Load(filename);
+   if(surf == NULL)
+   {
+      printf("Error: could not load %s\n", filename);
+      return NULL;
+   }
+
+   text = SDL_CreateTextureFromSurface(rend, surf);
+
+   SDL_FreeSurface(surf);
+   return text;
+
+}
+
+static void draw_at(SDL_Renderer * rend, SDL_Texture * text, int imgid, int x, int y)
+{   
+   SDL_Rect r_src;
+   SDL_Rect r_dest;
+   int ix, iy;
+   
+   r_dest.x = x;
+   r_dest.y = y;
+   r_dest.w = TILE_WIDTH;
+   r_dest.h = TILE_HEIGHT;
+
+
+   ix = imgid & 0xFF;
+   iy = (imgid >> 8) & 0xFF;
+
+   r_src.x = ix * TILE_WIDTH;
+   r_src.y = iy * TILE_HEIGHT;
+   r_src.w = TILE_WIDTH;
+   r_src.h = TILE_HEIGHT;
+
+   SDL_RenderCopy(rend, text, &r_src, &r_dest);
+}
+
+
